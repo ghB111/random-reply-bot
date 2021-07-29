d13159eebc6bd15549793db0076883a611d790fb
Roman <b111forjob@gmail.com>
2021-07-29 21:23:46 +0700

    Go for TypeScript

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,9 @@ const fs = require('fs');
 const token = fs.readFileSync('.token', 'utf8');
 const replies = fs.readFileSync('replies.txt', 'utf8').split('\n');
 
-console.log(token, replies);
-
 const bot = new TelegramBot(token, {polling: true});
 
-bot.on('message', (msg) => {
+bot.on('message', (msg: { chat: { id: any; }; }) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, replies[Math.floor(Math.random()*replies.length)]);
 });
